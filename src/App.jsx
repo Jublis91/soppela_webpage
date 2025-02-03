de40211ad8e6dc447d46dc9d6e11a8ccd77be352
Juuso Nikander <juusonikander@kamk.fi>
2025-02-03 21:01:38 +0200

    navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Home from "./pages/home";
+import Soppela from "./pages/soppela";
+import Me from "./pages/me";
+import Portfolio from "./pages/portfolio";
+
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
+    <Router>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        {/* Navigaatiopalkki */}
+        <nav>
+          <ul>
+            <li><Link to="/">Etusivu</Link></li>
+            <li><Link to="/me">Minä</Link></li>
+            <li><Link to="/portfolio">Portfolio</Link></li>
+            <li><Link to="/soppela">Soppela</Link></li>
+          </ul>
+        </nav>
+
+        {/* Reititys */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/me" element={<Me />} />
+          <Route path="/portfolio" element={<Portfolio />} />
+          <Route path="/soppela" element={<Soppela />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </Router>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/soppela.jsx
@@ -0,0 +1,9 @@
+function Soppela() {
+  return (
+    <div>
+      <h1>Soppela</h1>
+    </div>
+  );
+}
+
+export default Soppela;
