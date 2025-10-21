8fdd73437faff232a13f16e5b35ff8e5e245d05d
Juuso Nikander <juusonikander@kamk.fi>
2025-10-21 20:02:18 +0300

    vite.config päivitetty

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const defaultProxyTarget = process.env.NODE_ENV === 'production'
-  ? 'http://api:3001'
-  : 'http://localhost:3001'
+const defaultProxyTarget =
+  process.env.NODE_ENV === 'production'
+    ? 'http://api:3001'
+    : 'http://soppela_prod:3001' 
 
 const proxyTarget = process.env.VITE_PROXY_TARGET ?? defaultProxyTarget
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,                
+    host: true,
+    port: 5173,
     strictPort: true,
-    watch: { usePolling: true }, 
+    watch: { usePolling: true },
 
     proxy: {
       '/api': {
-        target: proxyTarget,   
+        target: proxyTarget,
+        changeOrigin: true,
+        secure: false,
+        rewrite: path => path.replace(/^\/api/, '/api')
+      },
+      '/uploads': {
+        target: proxyTarget,
         changeOrigin: true,
         secure: false
       },
